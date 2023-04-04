ca673891a6fe160cabf837c89813cbced3bbca7b
Bodula-sravani <sravanibodula@gmail.com>
2023-04-04 17:53:49 +0530

    Test case for drivers license methd

---
+++ driversLicenseTests/driversLicenseTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assessment2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace License.Tests
+{
+    [TestClass()]
+    public class LicenseTests
+    {
+        [TestMethod()]
+        public void driversLicenseTest()
+        {
+            driversLicenseClass p = new();
+            int time = p.driversLicense("Eric", 2, "Adam Caroline Rebecca Frank");
+            if (time == 40)
+            {
+                Assert.IsTrue(true);
+            }
+            else { Assert.Fail(); }
+        }
+    }
+}
